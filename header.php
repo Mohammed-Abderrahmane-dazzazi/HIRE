9939d15017c24a6dc582ffbffb5f6a658e33989a
Mohammed-Abderrahmane-dazzazi <126981580+Mohammed-Abderrahmane-dazzazi@users.noreply.github.com>
2023-03-20 00:04:25 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,58 @@
+<header id="header">
+
+			<nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function">
+
+				<div class="container">
+					
+					<div class="logo-wrapper">
+						<div class="logo">
+							 <img src="../images/01.jpg" alt="Logo" /> 
+						</div>
+					</div>
+					
+					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">
+					
+						<ul class="nav navbar-nav" id="responsive-menu">
+						
+							<li>
+							
+								<a href="../">Home</a>
+								
+							</li>
+							
+							<li>
+								<a href="../job-list.php">Job List</a>
+
+							</li>
+							
+							<li>
+								<a href="../employers.php">Employers</a>
+							</li>
+							
+							<li>
+								<a href="../employees.php">Employees</a>
+							</li>
+							
+							<li>
+								<a href="../contact.php">Contact Us</a>
+							</li>
+
+						</ul>
+				
+					</div>
+
+					<div class="nav-mini-wrapper">
+						<ul class="nav-mini sign-in">
+							<li><a href="../logout.php">logout</a></li>
+							<li><a href="./">Profile</a></li>
+						</ul>
+					</div>
+				
+				</div>
+				
+				<div id="slicknav-mobile"></div>
+				
+			</nav>
+
+			
+		</header>
