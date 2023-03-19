a51067834f8ac5336647a589b3b0036a91990e7a
Mohammed-Abderrahmane-dazzazi <126981580+Mohammed-Abderrahmane-dazzazi@users.noreply.github.com>
2023-03-19 23:53:39 +0100

    Add files via upload

---
+++ constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = 'Morocco/Casablanca';
+$smtp_host = 'smtp.gmail.com';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = 'belamhaoualamine@gmail.com';
+
+$fb  = 'https://www.facebook.com';
+$tw = 'https://twitter.com';
+$ig = 'https://www.instagram.com';
+?>
